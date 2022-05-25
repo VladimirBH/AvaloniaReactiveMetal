be74e234599caa118a36909fea4bb7d422988900
VladimirBH <vladimirburbah07@gmail.com>
2022-05-26 00:20:50 +0300

    Initial commit

---
+++ AvaloniaReactiveMetal/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using A.Views;
+using AvaloniaClientMetal.Models;
+using MessageBox.Avalonia.Enums;
+
+namespace A.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            LoadingApplication();
+            
+        }
+
+        private static async void LoadingApplication()
+        {
+            try
+            {
+                PreparedLocalStorage.LoadLocalStorage();
+                TokenPair tokenPair = PreparedLocalStorage.GetTokenPairFromLocalStorage();
+                tokenPair = await UserImplementation.RefreshTokenPair(tokenPair.RefreshToken);
+                PreparedLocalStorage.PutTokenPairFromLocalStorage(tokenPair);
+                KeepRoleId.RoleId = tokenPair.IdRole;
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                var messageBox = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow("Успех",
+                    ex.Message+"\t", ButtonEnum.Ok, MessageBox.Avalonia.Enums.Icon.Success);
+                await messageBox.Show();
+            }
+        }
+    }
+}
